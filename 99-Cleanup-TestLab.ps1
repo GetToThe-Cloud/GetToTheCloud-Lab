ee81f9b878d448abf4a8050d756dcdb31a39d8ef
buzzict <info@buzz-ict.nl>
2022-05-05 17:25:11 +0200

    update

--- 99-Cleanup-TestLab.ps1
+++ 99-Cleanup-TestLab.ps1
@@ -1,15 +1,15 @@
 function Load-Module ($m) {
 
     # If module is imported say that and do nothing
-    if (Get-Module | Where-Object {$_.Name -eq $m}) {
+    if (Get-Module | Where-Object { $_.Name -eq $m }) {
         write-host "[INFO] Module $m is already imported."
     }
     else {
 
         # If module is not imported, but available on disk then import
-        if (Get-Module -ListAvailable | Where-Object {$_.Name -eq $m}) {
-            if ($M -eq "MSOnline"){
-            Import-Module $m  -UseWindowsPowershell 
+        if (Get-Module -ListAvailable | Where-Object { $_.Name -eq $m }) {
+            if ($M -eq "MSOnline") {
+                Import-Module $m  -UseWindowsPowershell 
             }
             else {
                 Import-Module $m
@@ -18,7 +18,7 @@ function Load-Module ($m) {
         else {
 
             # If module is not imported, not available on disk, but is in online gallery then install and import
-            if (Find-Module -Name $m | Where-Object {$_.Name -eq $m}) {
+            if (Find-Module -Name $m | Where-Object { $_.Name -eq $m }) {
                 Install-Module -Name $m -Force -Verbose -Scope CurrentUser
                 Import-Module $m 
             }
@@ -50,7 +50,7 @@ Connect-MSOLService
 Write-Host "[INFO] Setting MsolDirSync to Disabled"
 Try {
     Set-MsolDirSyncEnabled -EnableDirSync $false -force
-    if (!((Get-MsolCompanyInformation).DirectorySynchronizationEnabled)){
+    if (!((Get-MsolCompanyInformation).DirectorySynchronizationEnabled)) {
         Write-Host "[SUCCESS] MsolDirSync is Disabled" -ForegroundColor Green
     }
     else {
@@ -62,41 +62,47 @@ catch {
     #do nothing
 }
 
-$Groups = Get-AZAdGroup | Where-Object {$_.Displayname -like "A-*" -or $_.DisplayName -like "GG-*" -or $_.DisplayName -Like "DG-*" -or $_.DisplayName -like "UG-*"} 
-$Users = Get-MSOLUser | Where-Object {$_.immutableID -ne $null}
+$Groups = Get-AZAdGroup | Where-Object { $_.Displayname -like "A-*" -or $_.DisplayName -like "GG-*" -or $_.DisplayName -Like "DG-*" -or $_.DisplayName -like "UG-*" } 
+$Users = Get-MSOLUser | Where-Object { $_.immutableID -ne $null }
 
 Write-Host "[INFO] there are $($Goups.Count) groups to be deleted and $($users.count) users"
 Write-Host "[INFO] Deleting synced groups ..."
-ForEach ($Group in $Groups){
-   if (Get-DistributionGroup $Group.DisplayName){
-       $group | Remove-DistributionGroup -confirm:$false
-       Write-Host "[WARNING] $($Group.Displayname) was a Distribution Group and is deleted" -ForegroundColor Yellow
-   }
-   Else {
-       $Group | Remove-AzADGroup
-       Write-Host "[WARNING] $($Group.Displayname) was a Security Group and is deleted" -ForegroundColor Yellow
-   }
+ForEach ($Group in $Groups) {
+    if (Get-DistributionGroup $Group.DisplayName) {
+        Remove-DistributionGroup $Group.Displayname -confirm:$false
+        Write-Host "[WARNING] $($Group.Displayname) was a Distribution Group and is deleted" -ForegroundColor Yellow
+    }
+    Else {
+        $Group | Remove-AzADGroup
+        Write-Host "[WARNING] $($Group.Displayname) was a Security Group and is deleted" -ForegroundColor Yellow
+    }
 }
 
 Write-Host "[INFO] Deleting synced users ..."
-ForEach ($User in $Users){
+ForEach ($User in $Users) {
     $user | Remove-MSOLUser -Force
     Write-Host "[WARNING] $($User.DisplayName) is deleted" -ForegroundColor Yellow
 }
-$ResourceGroupName = Get-AZResourceGroup | Where-Object {$_.ResourceGroupName -like "GetToTheCloud*"}
-Write-Host "[INFO] Deleting everything in resourcegroup: $($ResourceGroupName.ResourceGroupName)" -ForegroundColor Yellow
-$ResourceGroupName | Remove-AZResourceGroup -Force
+$ResourceGroupName = Get-AZResourceGroup | Where-Object { $_.ResourceGroupName -like "GetToTheCloud*" }
+If (!($ResourceGroupName)) {
+    Write-Host "[INFO] No ResourceGroupName with the name $ResourceGroupName is present"
+}
+else {
+    
+    Write-Host "[INFO] Deleting everything in resourcegroup: $($ResourceGroupName.ResourceGroupName)" -ForegroundColor Yellow
+    $ResourceGroupName | Remove-AZResourceGroup -Force
+}
 
 Write-Host "[INFO] Checking if everything is deleted"
-$Groups = Get-AZAdGroup | Where-Object {$_.Displayname -like "A-*" -or $_.DisplayName -like "GG-*" -or $_.DisplayName -Like "DG-*" -or $_.DisplayName -like "UG-*"} 
-$Users = Get-MSOLUser | Where-Object {$_.immutableID -ne $null}
-$ResourceGroupName = Get-AZResourceGroup | Where-Object {$_.ResourceGroupName -like "GetToTheCloud*"}
-if (!($Groups)){
+$Groups = Get-AZAdGroup | Where-Object { $_.Displayname -like "A-*" -or $_.DisplayName -like "GG-*" -or $_.DisplayName -Like "DG-*" -or $_.DisplayName -like "UG-*" } 
+$Users = Get-MSOLUser | Where-Object { $_.immutableID -ne $null }
+$ResourceGroupName = Get-AZResourceGroup | Where-Object { $_.ResourceGroupName -like "GetToTheCloud*" }
+if (!($Groups)) {
     Write-Host "[SUCCESS] all groups related to testlab is deleted" -ForegroundColor green
 }
-if (!($users)){
+if (!($users)) {
     Write-Host "[SUCCESS] all users related to testlab is deleted" -ForegroundColor green
 }
-if (!($ResourceGroupName)){
+if (!($ResourceGroupName)) {
     Write-Host "[SUCCESS] all Resources related to testlab is deleted" -ForegroundColor green
 }
