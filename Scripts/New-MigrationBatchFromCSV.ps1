cb1a14fae87fd891fab83e0dd461150c35b67757
buzzict <info@buzz-ict.nl>
2022-05-08 21:18:47 +0200

    update

--- Scripts/New-MigrationBatchFromCSV.ps1
+++ Scripts/New-MigrationBatchFromCSV.ps1
@@ -5,6 +5,7 @@
 ## PREPARING USERS TO MIGRATE
 $ExchangeServer = $env:Computername
 $ExchangeServer = (Get-ADComputer $ExchangeServer).DNSHostname
+$TargetDeliveryDomain = ""
 
 Write-Host "[INFO] Connecting to $($ExchangeServer) ..."
 ## connection to exchange
@@ -16,10 +17,12 @@ If (!(Get-Pssession | where-Object { $_.ConfigurationName -eq "Microsoft.Exchang
     break
 }
 
+$migrationEndpoint = Get-MigrationEndpoint
 
 $Batches = Get-Childitem C:\Temp | Where-Object { $_.Name -like “batch*.txt” }
 $Batches = $batches.Split(“.”)[0];
 ForEach ($Batch in $Batches) {
     $Users = Get-Content "C:\Temp\$Batch.txt"
-    New-MigrationBatch -Name $Batch -SourceEndpoint $SOURCEPoint -TargetDeliveryDomain $Target -CSVData ([System.IO.File]::ReadAllBytes("C:\temp\$Batch.csv"))
+    $Batch = New-MigrationBatch -Name $Batch -SourceEndpoint $MigrationEndpoint.Identity -TargetDeliveryDomain $TargetDeliveryDomain -CSVData ([System.IO.File]::ReadAllBytes("C:\temp\$Batch.csv"))
+    Start-MigrationBatch -Identity $Batch.Identity
 }
